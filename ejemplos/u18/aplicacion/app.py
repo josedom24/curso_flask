1ab61bc5f22e5fb5d07f49f982ba334a030ef4d4
José Domingo <josedom24@gmail.com>
2017-06-05 13:48:34 +0200

    u18

--- ejemplos/u18/aplicacion/app.py
+++ ejemplos/u18/aplicacion/app.py
@@ -21,6 +21,39 @@ def procesar_formulario():
 	else:
 		return render_template("error.html",error="Contraseña incorrecta")
 
+@app.route("/calculadora", methods=["get","post"])
+def calculadora():
+	if request.method=="POST":
+		num1=request.form.get("num1")
+		num2=request.form.get("num2")
+		operador=request.form.get("operador")
+	
+		if operador=="+":
+			try:
+				resultado=int(num1)+int(num2)
+			except:
+				return render_template("error.html",error="No puedo realizar la operación")
+		if operador=="-":
+			try:
+				resultado=int(num1)-int(num2)
+			except:
+				return render_template("error.html",error="No puedo realizar la operación")
+		if operador=="*":
+			try:
+				resultado=int(num1)*int(num2)
+			except:
+				return render_template("error.html",error="No puedo realizar la operación")
+		if operador=="/":
+			try:
+				resultado=int(num1)/int(num2)
+			except:
+				return render_template("error.html",error="No puedo realizar la operación")
+
+		return render_template("resultado.html",num1=num1,num2=num2,operador=operador,resultado=resultado)	
+	else:
+		return render_template("calculadora.html")		
+
+
 @app.errorhandler(404)
 def page_not_found(error):
 	return render_template("error.html",error="Página no encontrada..."), 404
