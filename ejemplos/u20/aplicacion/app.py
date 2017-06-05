59f841d0ba7d1f0b8390f9894d93b684b2fd3756
josedom24 <josedom24@gmail.com>
2017-06-06 00:19:12 +0200

    u20

---
+++ ejemplos/u20/aplicacion/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from os import listdir
+
+
+
+
+app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+Bootstrap(app)	
+
+@app.route('/')
+def inicio():
+	
+	lista=[]
+	for file in listdir(app.root_path+"/static/img"):
+		lista.append(file)
+	return render_template("inicio.html",lista=lista)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template("error.html",error="Página no encontrada..."), 404
+
+if __name__ == '__main__':
+	app.run('0.0.0.0',8080, debug=True)
