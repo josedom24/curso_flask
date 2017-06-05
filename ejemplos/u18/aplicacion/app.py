4ab3cec3be55d1370ae18811e8083e09109988b0
José Domingo <josedom24@gmail.com>
2017-06-05 09:24:53 +0200

    u18

--- ejemplos/u18/aplicacion/app.py
+++ ejemplos/u18/aplicacion/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)	
@@ -7,8 +7,19 @@ Bootstrap(app)
 def inicio():
     return render_template("inicio.html")
 
+@app.route('/formulario')
+def formulario():
+    return render_template("formulario.html")
 
 
+@app.route("/procesar", methods=["post"])
+def procesar_formulario():
+	passwd=request.form.get("pass_control")
+	if passwd=="asdasd":
+		datos = request.form
+		return render_template("datos.html",datos=datos)	
+	else:
+		return render_template("error.html",error="Contraseña incorrecta")
 
 @app.errorhandler(404)
 def page_not_found(error):
