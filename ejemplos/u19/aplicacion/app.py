e829ff8b6830a2fdfabf0d212bef19a1a761dffa
José Domingo <josedom24@gmail.com>
2017-06-06 08:49:17 +0200

    u19

--- ejemplos/u19/aplicacion/app.py
+++ ejemplos/u19/aplicacion/app.py
@@ -27,12 +27,11 @@ def procesar_formulario():
 def calculadora_post():
 	form=formcalculadora(request.form)
 	if form.validate_on_submit():
-		num1=request.form.get("num1")
-		num2=request.form.get("num2")
-		operador=request.form.get("operador")
-	
+		num1=form.num1.data
+		num2=form.num2.data
+		operador=form.operador.data
 		try:
-			resultado=eval(num1+operador+num2)
+			resultado=eval(str(num1)+operador+str(num2))
 		except:
 			return render_template("error.html",error="No puedo realizar la operación")
 		
