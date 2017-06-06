eb09a55a1b1f24bc82025a54fc679e84fcbe137f
josedom24 <josedom24@gmail.com>
2017-06-06 20:56:11 +0200

    u19

--- ejemplos/u19/aplicacion/app.py
+++ ejemplos/u19/aplicacion/app.py
@@ -9,19 +9,6 @@ Bootstrap(app)
 def inicio():
     return render_template("inicio.html")
 
-@app.route('/formulario')
-def formulario():
-    return render_template("formulario.html")
-
-
-@app.route("/procesar", methods=["post"])
-def procesar_formulario():
-	passwd=request.form.get("pass_control")
-	if passwd=="asdasd":
-		datos = request.form
-		return render_template("datos.html",datos=datos)	
-	else:
-		return render_template("error.html",error="Contraseña incorrecta")
 
 @app.route("/calculadora_post", methods=["get","post"])
 def calculadora_post():
