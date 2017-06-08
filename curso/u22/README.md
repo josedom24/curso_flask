5bde74b7a36263f1052a8c655e79e74b03defa4f
José Domingo <josedom24@gmail.com>
2017-06-08 14:36:08 +0200

    u22

--- curso/u22/README.md
+++ curso/u22/README.md
@@ -13,30 +13,35 @@ Fichero model.py
 ## Creación de datos de prueba en nuetra base de datos
 
 
-juegos=[
-    {"nombre":"PAC-MAN","precio":12,"descripcion":"juego de fantasmitas","stock":1,"CategoriaId":1},
-    {"nombre":"Super Mario Bros","precio":25,"descripcion":"juego de plataforma","stock":5,"CategoriaId":1}
-    ]
 
-
-    In [1]: from aplicacion.app import db
+from aplicacion.app import db
+from aplicacion.model import Categorias,Articulos,Usuarios
 
 db.create_all()
-
-from aplicacion.model import Categorias,Articulos
-
 cat=Categorias(nombre="Arcade")
-
 db.session.add(cat)
+db.session.commit()
 
-art1=Articulos(nombre="PAC-MAN",precio=12,descripcion="juego de fantasmitas",stock=1,CategoriaId
-    ...: =1)
+art1=Articulos(nombre="PAC-MAN",precio=12,descripcion="juego de fantasmitas",stock=1,CategoriaId=1)
 
-In [12]: 
+art2=Articulos(nombre="Super Mario Bros",precio=25,descripcion="juego de platoformas",stock=10,caetgoria=cat)
 
-In [12]: art2=Articulos(nombre="Super Mario Bros",precio=25,descripcion="juego de platoformas",stock=10,C
-    ...: ategoriaId=1)
+db.session.add_all([art1,art2])
 
-In [13]: db.session.add_all([art1,art2])
+db.session.commit()
 
-In [14]: db.session.commit()
+
+art=Articulos.query.first()
+art=Articulos.query.get(2)
+
+Articulos.query.count()
+2
+
+Articulos.query.filter_by(precio=25).all()
+
+for art in Articulos.query.all():
+   ...:     print (art.nombre)
+   ...:     
+PAC-MAN
+Super Mario Bros
+
--- ejemplos/u22/aplicacion/model.py
+++ ejemplos/u22/aplicacion/model.py
@@ -1,22 +1,22 @@
 from datetime import datetime
 from sqlalchemy import Boolean, Column , ForeignKey
 from sqlalchemy import DateTime, Integer, String, Text, Float
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-Base = declarative_base()
+from aplicacion.app import db
 
-class Categorias(Base):
+
+class Categorias(db.Model):
 	"""Categorías de los artículos"""
 	__tablename__ = 'categorias'
 	id = Column(Integer, primary_key=True)
 	nombre = Column(String(100))
-	
+	articulos = relationship("Articulos", backref="Categorias",lazy='dynamic')
 
 
 	def __repr__(self):
 		return (u'<{self.__class__.__name__}: {self.id}>'.format(self=self))
 
-class Articulos(Base):
+class Articulos(db.Model):
 	"""Artículos de nuestra tienda"""
 	__tablename__ = 'articulos'
 	id = Column(Integer, primary_key=True)
@@ -29,12 +29,15 @@ class Articulos(Base):
 	CategoriaId=Column(Integer,ForeignKey('categorias.id'), nullable=False)
 	categoria = relationship("Categorias", backref="Articulos")
 
+	def precio_final(self):
+		return self.precio*self.iva/100
+
 	def __repr__(self):
 		return (u'<{self.__class__.__name__}: {self.id}>'.format(self=self))
 
 
 
-class Usuarios(Base):
+class Usuarios(db.Model):
 	"""Usuarios"""
 	__tablename__ = 'usuarios'
 	id = Column(Integer, primary_key=True)
@@ -45,40 +48,7 @@ class Usuarios(Base):
 	activo = Column(Boolean, default=False)
 	email = Column(String(200),nullable=False)
 	admin = Column(Boolean, default=False)
-
+	
 	def __repr__(self):
 		return (u'<{self.__class__.__name__}: {self.id}>'.format(self=self))
 
-if __name__ == '__main__':  # pragma: no cover
-    from datetime import timedelta
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-
-    engine = create_engine('sqlite:///../dbase.db', echo=True)
-
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    categorias = ("Deportes","Arcade","Carreras","Acción")
-
-    for cat in categorias:
-    	categoria=Categorias(nombre=cat)
-    	session.add(categoria)
-    	session.commit()
-
-    juegos=[
-    {"nombre":"Fifa 2017","precio":65,"descripcion":"juego de futbol","stock":10,"CategoriaId":1},
-    {"nombre":"PES 2017","precio":35,"descripcion":"juego de futbol","stock":7,"CategoriaId":1},
-    {"nombre":"PAC-MAN","precio":12,"descripcion":"juego de fantasmitas","stock":1,"CategoriaId":2},
-    {"nombre":"Super Mario Bros","precio":25,"descripcion":"juego de plataforma","stock":5,"CategoriaId":2},
-    {"nombre":"Need for Spped","precio":25,"descripcion":"juego de carreras","stock":10,"CategoriaId":3},
-    {"nombre":"Out Run","precio":15,"descripcion":"juego de coches","stock":3,"CategoriaId":3},
-    {"nombre":"Destiny","precio":75,"descripcion":"juego de disparos y acción","stock":8,"CategoriaId":4},
-    {"nombre":"Metal Gear","precio":65,"descripcion":"juego de acción","stock":10,"CategoriaId":4},
-    
-    ]
-    for jue in juegos:
-       	juego=Articulos(**jue)
-       	session.add(juego)
-       	session.commit()
