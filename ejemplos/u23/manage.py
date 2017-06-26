b5c6f3e38bf67671fc337994437f17fa3a7b55ae
josedom24 <josedom24@gmail.com>
2017-06-26 19:11:40 +0200

    Nuevas imágenes

--- ejemplos/u23/manage.py
+++ ejemplos/u23/manage.py
@@ -9,9 +9,6 @@ app.config['DEBUG'] = True # Ensure debugger will load.
 def create_tables():
     "Create relational database tables."
     db.create_all()
-    categoria=Categorias(id=0,nombre="Todos")
-    db.session.add(categoria)
-    db.session.commit()
 
 @manager.command
 def drop_tables():
@@ -22,7 +19,6 @@ def drop_tables():
 def add_data_tables():
     db.create_all()
 
-    
     categorias = ("Deportes","Arcade","Carreras","Acción")
 
     for cat in categorias:
@@ -31,14 +27,14 @@ def add_data_tables():
     	db.session.commit()
 
     juegos=[
-    {"nombre":"Fifa 2017","precio":65,"descripcion":"juego de futbol","image":"fifa07.jpg","stock":10,"CategoriaId":1},
-    {"nombre":"PES 2017","precio":35,"descripcion":"juego de futbol","image":"pes2017.jpg","stock":7,"CategoriaId":1},
-    {"nombre":"PAC-MAN","precio":12,"descripcion":"juego de fantasmitas","image":"pacman.jpg","stock":1,"CategoriaId":2},
-    {"nombre":"Super Mario Bros","precio":25,"descripcion":"juego de plataforma","image":"supermariobros.png","stock":5,"CategoriaId":2},
-    {"nombre":"Need for Spped","precio":25,"descripcion":"juego de carreras","image":"needforspeed.jpg","stock":10,"CategoriaId":3},
-    {"nombre":"Out Run","precio":15,"descripcion":"juego de coches","image":"outrun.jpg","stock":3,"CategoriaId":3},
-    {"nombre":"Destiny","precio":75,"descripcion":"juego de disparos y acción","image":"destiny.jpg","stock":8,"CategoriaId":4},
-    {"nombre":"Metal Gear","precio":65,"descripcion":"juego de acción","image":"metalgear.jpg","stock":10,"CategoriaId":4},
+    {"nombre":"Fernando Martín Basket","precio":65,"descripcion":"Fernando Martín Basket Master es un videojuego de baloncesto, uno contra uno, publicado por Dinamic Software en 1987","stock":10,"CategoriaId":1,,"image":"basket.jpeg"},
+    {"nombre":"Hyper Soccer","precio":35,"descripcion":"Konami Hyper Soccer fue el primer videojuego de fútbol de Konami para una consola Nintendo, y considerado la semilla de las posteriores sagas International Superstar Soccer y Winning Eleven.","stock":7,"CategoriaId":1,"image":"soccer.jpeg"},
+    {"nombre":"Arkanoid","precio":12,"descripcion":"Arkanoid es un videojuego de arcade desarrollado por Taito en 1986. Está basado en los Breakout de Atari de los años 70.","stock":1,"CategoriaId":2,"image":"arkanoid.jpeg"},
+    {"nombre":"Tetris","precio":18,"descripcion":"Tetris es un videojuego de puzzle originalmente diseñado y programado por Alekséi Pázhitnov en la Unión Soviética.","stock":5,"CategoriaId":2,"image":"tetris.jpeg"},
+    {"nombre":"Road Fighter","precio":25,"descripcion":"Road Fighter es un videojuego de carreras producido por Konami y lanzado en los arcades en 1984. Fue el primer juego de carreras desarrollado por esta compañía.","stock":10,"CategoriaId":3,"image":"road.jpeg"},
+    {"nombre":"Out Run","precio":15,"descripcion":"Out Run es un videojuego de carreras creado en 1986 por Yū Suzuki y Sega-AM2, y publicado inicialmente para máquinas recreativas.","stock":3,"CategoriaId":3,"image":"outrun.jpeg"},
+    {"nombre":"Army Moves","precio":23,"descripcion":"Army Moves es un arcade y primera parte de la trilogía Moves diseñado por Víctor Ruiz, de Dinamic Software para Commodore Amiga, Amstrad CPC, Atari ST, Commodore 64, MSX y ZX Spectrum en 1986.","stock":8,"CategoriaId":4,"image":"army.jpeg"},
+    {"nombre":"La Abadia del Crimen","precio":65,"descripcion":"La Abadía del Crimen es un videojuego desarrollado inicialmente de forma freelance y publicado por la Academia Mister Chip en noviembre de 1987, posteriormente se publica bajo el sello de Opera Soft ya entrado 1988.","stock":10,"CategoriaId":4,"image":"abadia.jpeg"},
     
     ]
     for jue in juegos:
