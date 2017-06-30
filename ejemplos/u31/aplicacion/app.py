3aa0c9d2b6ccead138a2871ff2e4cd645a9ef8fe
josedom24 <josedom24@gmail.com>
2017-06-30 21:47:38 +0200

    u31

--- ejemplos/u31/aplicacion/app.py
+++ ejemplos/u31/aplicacion/app.py
@@ -233,19 +233,70 @@ def carrito_add(id):
 	form=formCarrito()
 	form.id.data=id
 	if form.validate_on_submit():
-		resp = make_response(redirect('inicio'))
-		data=[{"id":1,"cant":2},{"id":3,"cant":4}]
-		resp.set_cookie('id',json.dumps(data))
-		return resp
+		if art.stock>=int(form.cantidad.data):
+			try:
+				datos = json.loads(request.cookies.get(str(current_user.id)))
+			except:
+				datos = []
+			actualizar= False
+			for dato in datos:
+				if dato["id"]==id:
+					dato["cantidad"]=form.cantidad.data
+					actualizar = True
+			if not actualizar:
+				datos.append({"id":form.id.data,"cantidad":form.cantidad.data})
+			resp = make_response(redirect(url_for('inicio')))
+			resp.set_cookie(str(current_user.id),json.dumps(datos))
+			return resp
+		form.cantidad.errors.append("No hay artículos suficientes.")
 	return render_template("carrito_add.html",form=form,art=art)
-@app.route('/prueba2')
-def prueba2():
-	datos = json.loads(request.cookies.get('id'))
-	r=""
-	for d in datos:
-		r=r+str(d["id"])+","+str(d["cant"])
-	return r
+@app.route('/carrito')
+@login_required
+def carrito():
+	try:
+		datos = json.loads(request.cookies.get(str(current_user.id)))
+	except:
+		datos = []
+	articulos=[]
+	cantidades=[]
+	total=0
+	for articulo in datos:
+		articulos.append(Articulos.query.get(articulo["id"]))
+		cantidades.append(articulo["cantidad"])
+		total=total+Articulos.query.get(articulo["id"]).precio_final()*articulo["cantidad"]
+	articulos=zip(articulos,cantidades)
+	return render_template("carrito.html",articulos=articulos,total=total)
 
+@app.route('/carrito_delete/<id>')
+@login_required
+def carrito_delete(id):
+	try:
+		datos = json.loads(request.cookies.get(str(current_user.id)))
+	except:
+		datos = []
+	new_datos=[]
+	for dato in datos:
+		if dato["id"]!=id:
+			new_datos.append(dato)
+	resp = make_response(redirect(url_for('carrito')))
+	resp.set_cookie(str(current_user.id),json.dumps(new_datos))
+	return resp
+
+@app.context_processor
+def contar_carrito():
+	if not current_user.is_authenticated:
+		return {'num_articulos':0}
+	if request.cookies.get(str(current_user.id))==None:
+		return {'num_articulos':0}
+	else:
+		datos = json.loads(request.cookies.get(str(current_user.id)))
+		return {'num_articulos':len(datos)}
+
+@app.route('/pedido')
+@login_required
+def pedido():
+	pass
+	
 @app.errorhandler(404)
 def page_not_found(error):
 	return render_template("error.html",error="Página no encontrada..."), 404
