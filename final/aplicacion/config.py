a66f81d18c30f8f6c3b31023921604a0f760cd53
josedom24 <josedom24@gmail.com>
2017-08-12 18:46:23 +0200

    Quito el directorio final, es antiguo

--- final/aplicacion/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Get the current working directory to place dnase.db during development.
-# In production, use absolute paths or a database management system.
-PWD = os.path.abspath(os.curdir)
-
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
