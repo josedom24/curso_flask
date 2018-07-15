afe2d0edac2e7980563f6243e177c3d54320a88a
crisconru <crisconru@gmail.com>
2018-07-15 20:36:17 +0200

    PEP8 u15

--- ejemplos/u15/manage.py
+++ ejemplos/u15/manage.py
@@ -1,9 +1,8 @@
 from flask_script import Manager
 from aplicacion.app import app
-	
-manager = Manager(app)
-app.config['DEBUG'] = True # Ensure debugger will load.
-	
-if __name__ == '__main__':
-	manager.run()
 
+manager = Manager(app)
+app.config['DEBUG'] = True  # Ensure debugger will load.
+
+if __name__ == '__main__':
+    manager.run()
