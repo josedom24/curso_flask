bd5bc972cfdc3acb458bc0d851d9b2ed7b5c318b
crisconru <crisconru@gmail.com>
2018-07-15 20:36:54 +0200

    PEP8 u18

--- ejemplos/u18/aplicacion/app.py
+++ ejemplos/u18/aplicacion/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
-Bootstrap(app)	
+Bootstrap(app)
+
 
 @app.route('/')
 def inicio():
     return render_template("inicio.html")
+
 
 @app.route('/formulario')
 def formulario():
@@ -14,58 +16,59 @@ def formulario():
 
 @app.route("/procesar", methods=["post"])
 def procesar_formulario():
-	passwd=request.form.get("pass_control")
-	if passwd=="asdasd":
-		datos = request.form
-		return render_template("datos.html",datos=datos)	
-	else:
-		return render_template("error.html",error="Contraseña incorrecta")
+    passwd = request.form.get("pass_control")
+    if passwd == "asdasd":
+        datos = request.form
+        return render_template("datos.html", datos=datos)
+    else:
+        return render_template("error.html", error="Contraseña incorrecta")
 
-@app.route("/calculadora_post", methods=["get","post"])
+
+@app.route("/calculadora_post", methods=["get", "post"])
 def calculadora_post():
-	if request.method=="POST":
-		num1=request.form.get("num1")
-		num2=request.form.get("num2")
-		operador=request.form.get("operador")
-	
-		try:
-			resultado=eval(num1+operador+num2)
-		except:
-			return render_template("error.html",error="No puedo realizar la operación")
-		
-		return render_template("resultado.html",num1=num1,num2=num2,operador=operador,resultado=resultado)	
-	else:
-		return render_template("calculadora_post.html")		
-
+    if request.method == "POST":
+        num1 = request.form.get("num1")
+        num2 = request.form.get("num2")
+        operador = request.form.get("operador")
+        try:
+            resultado = eval(num1 + operador + num2)
+        except:
+            return render_template("error.html",
+                                   error="No puedo realizar la operación")
+        return render_template("resultado.html", num1=num1, num2=num2,
+                               operador=operador, resultado=resultado)
+    else:
+        return render_template("calculadora_post.html")
 
 
 @app.route("/calculadora_get", methods=["get"])
 def calculadora_get():
-	if request.method=="GET" and len(request.args)>0:
-		num1=request.args.get("num1")
-		num2=request.args.get("num2")
-		operador=request.args.get("operador")
-	
-		try:
-			resultado=eval(num1+operador+num2)
-		except:
-			return render_template("error.html",error="No puedo realizar la operación")
-		
-		return render_template("resultado.html",num1=num1,num2=num2,operador=operador,resultado=resultado)	
-	else:
-		return render_template("calculadora_get.html")		
+    if request.method == "GET" and len(request.args) > 0:
+        num1 = request.args.get("num1")
+        num2 = request.args.get("num2")
+        operador = request.args.get("operador")
+        try:
+            resultado = eval(num1 + operador + num2)
+        except:
+            return render_template("error.html",
+                                   error="No puedo realizar la operación")
+        return render_template("resultado.html", num1=num1, num2=num2,
+                               operador=operador, resultado=resultado)
+    else:
+        return render_template("calculadora_get.html")
 
 
 @app.route("/calculadora/<operador>/<num1>/<num2>", methods=["get"])
-def calculadora_var(operador,num1,num2):
-	try:
-		resultado=eval(num1+operador+num2)
-	except:
-			return render_template("error.html",error="No puedo realizar la operación")
-	return render_template("resultado.html",num1=num1,num2=num2,operador=operador,resultado=resultado)	
+def calculadora_var(operador, num1, num2):
+    try:
+        resultado = eval(num1 + operador + num2)
+    except:
+            return render_template("error.html",
+                                   error="No puedo realizar la operación")
+    return render_template("resultado.html", num1=num1, num2=num2,
+                           operador=operador, resultado=resultado)
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-	return render_template("error.html",error="Página no encontrada..."), 404
-
+    return render_template("error.html", error="Página no encontrada..."), 404
