f34e234fb7d3b4945c3ad45e3e6c89003a171704
crisconru <crisconru@gmail.com>
2018-07-15 23:40:32 +0200

    PEP8 excepto app.py por los imports despues de db

--- ejemplos/u29/aplicacion/login.py
+++ ejemplos/u29/aplicacion/login.py
@@ -1,33 +1,38 @@
-from flask import session,redirect
+from flask import session, redirect
 from aplicacion.app import app
 
-def login_user(Usuario):
-	session["id"]=Usuario.id
-	session["username"]=Usuario.username
-	session["admin"]=Usuario.admin
+
+def login_user(usuario):
+    session["id"] = usuario.id
+    session["username"] = usuario.username
+    session["admin"] = usuario.admin
+
 
 def logout_user():
-	session.pop("id",None)
-	session.pop("username",None)
-	session.pop("admin",None)
+    session.pop("id", None)
+    session.pop("username", None)
+    session.pop("admin", None)
+
 
 def is_login():
-	if "id" in session:
-		return True
-	else:
-		return False
+    if "id" in session:
+        return True
+    else:
+        return False
+
 
 def is_admin():
-	return session.get("admin",False) 
+    return session.get("admin", False)
+
 
 @app.context_processor
 def login():
-	if "id" in session:
-		return {'is_login':True}
-	else:
-		return {'is_login':False}
+    if "id" in session:
+        return {'is_login': True}
+    else:
+        return {'is_login': False}
+
 
 @app.context_processor
 def admin():
-	return {'is_admin':session.get("admin",False) }
-	
+    return {'is_admin': session.get("admin", False)}
