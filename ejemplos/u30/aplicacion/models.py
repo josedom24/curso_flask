8191db0b45f96644b8128d3e2c7a00db19dd1c09
crisconru <crisconru@gmail.com>
2018-07-16 00:18:36 +0200

    PEP8 u30 excepto app.py por los import despues de db

--- ejemplos/u30/aplicacion/models.py
+++ ejemplos/u30/aplicacion/models.py
@@ -1,76 +1,78 @@
-from sqlalchemy import Boolean, Column , ForeignKey
+from sqlalchemy import Boolean, Column, ForeignKey
 from sqlalchemy import DateTime, Integer, String, Text, Float
 from sqlalchemy.orm import relationship
 from aplicacion.app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class Categorias(db.Model):
-	"""Categorías de los artículos"""
-	__tablename__ = 'categorias'
-	id = Column(Integer, primary_key=True)
-	nombre = Column(String(100))
-	articulos = relationship("Articulos", cascade="all, delete-orphan", backref="Categorias",lazy='dynamic')
+    """Categorías de los artículos"""
+    __tablename__ = 'categorias'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100))
+    articulos = relationship("Articulos", cascade="all, delete-orphan",
+                             backref="Categorias", lazy='dynamic')
 
+    def __repr__(self):
+        return (u'<{self.__class__.__name__}: {self.id}>'.format(self=self))
 
-	def __repr__(self):
-		return (u'<{self.__class__.__name__}: {self.id}>'.format(self=self))
 
 class Articulos(db.Model):
-	"""Artículos de nuestra tienda"""
-	__tablename__ = 'articulos'
-	id = Column(Integer, primary_key=True)
-	nombre = Column(String(100),nullable=False)
-	precio = Column(Float,default=0)
-	iva = Column(Integer,default=21)
-	descripcion = Column(String(255))
-	image = Column(String(255))
-	stock = Column(Integer,default=0)
-	CategoriaId=Column(Integer,ForeignKey('categorias.id'), nullable=False)
-	categoria = relationship("Categorias", backref="Articulos")
+    """Artículos de nuestra tienda"""
+    __tablename__ = 'articulos'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), nullable=False)
+    precio = Column(Float, default=0)
+    iva = Column(Integer, default=21)
+    descripcion = Column(String(255))
+    image = Column(String(255))
+    stock = Column(Integer, default=0)
+    CategoriaId = Column(Integer, ForeignKey('categorias.id'), nullable=False)
+    categoria = relationship("Categorias", backref="Articulos")
 
-	def precio_final(self):
-		return self.precio+(self.precio*self.iva/100)
+    def precio_final(self):
+        return self.precio+(self.precio*self.iva/100)
 
-	def __repr__(self):
-		return (u'<{self.__class__.__name__}: {self.id}>'.format(self=self))
+    def __repr__(self):
+        return (u'<{self.__class__.__name__}: {self.id}>'.format(self=self))
 
 
 class Usuarios(db.Model):
-	"""Usuarios"""
-	__tablename__ = 'usuarios'
-	id = Column(Integer, primary_key=True)
-	username = Column(String(100),nullable=False)
-	password_hash = Column(String(128),nullable=False)
-	nombre = Column(String(200),nullable=False)
-	email = Column(String(200),nullable=False)
-	admin = Column(Boolean, default=False)
-	
-	def __repr__(self):
-		return (u'<{self.__class__.__name__}: {self.id}>'.format(self=self))
+    """Usuarios"""
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(100), nullable=False)
+    password_hash = Column(String(128), nullable=False)
+    nombre = Column(String(200), nullable=False)
+    email = Column(String(200), nullable=False)
+    admin = Column(Boolean, default=False)
 
-	@property
-	def password(self):
-		raise AttributeError('password is not a readable attribute')
+    def __repr__(self):
+        return (u'<{self.__class__.__name__}: {self.id}>'.format(self=self))
 
-	@password.setter
-	def password(self, password):
-		self.password_hash = generate_password_hash(password)
-	def verify_password(self, password):
-		return check_password_hash(self.password_hash, password)
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
 
- 	
-# Flask-Login integration
-	def is_authenticated(self):
-		return True
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-	def is_active(self):
-		return True
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-	def is_anonymous(self):
-		return False
+    # Flask-Login integration
+    def is_authenticated(self):
+        return True
 
-	def get_id(self):
-		return str(self.id)
+    def is_active(self):
+        return True
 
-	def is_admin(self):
-		return self.admin
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
+    def is_admin(self):
+        return self.admin
