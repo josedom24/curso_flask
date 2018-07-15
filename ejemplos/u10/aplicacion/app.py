bf6d5c07d7278ff5819639aad3acf9c23934c727
crisconru <crisconru@gmail.com>
2018-07-15 19:40:20 +0200

    PEP8 u10

--- ejemplos/u10/aplicacion/app.py
+++ ejemplos/u10/aplicacion/app.py
@@ -1,17 +1,21 @@
 from flask import Flask, request
-app = Flask(__name__)	
+app = Flask(__name__)
+
 
 @app.route('/')
 def inicio():
     return 'Página principal'
 
+
 @app.route('/articulos/')
 def articulos():
     return 'Lista de artículos'
 
-@app.route('/articulos/new',methods=["POST"])
+
+@app.route('/articulos/new', methods=["POST"])
 def articulos_new():
-	return 'Está URL recibe información de un formulario con el método POST'
+    return 'Está URL recibe información de un formulario con el método POST'
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -19,4 +23,3 @@ def login():
         return 'Hemos accedido con POST'
     else:
         return 'Hemos accedido con GET'
-
